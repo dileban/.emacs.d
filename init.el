55374403641fd3f2a2a4d34b20add7ff6b678929
Dileban Karunamoorthy <dileban@gmail.com>
2018-06-29 22:10:00 +1000

    Switch to using 'use-package' to install and configure packages

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key))))
+ '(package-selected-packages (quote (ace-jump-mode use-package which-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
