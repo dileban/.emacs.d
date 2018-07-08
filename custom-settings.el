a3cf7571238ad16ce5b939b40320159706b92d9c
Dileban Karunamoorthy <dileban@gmail.com>
2018-07-08 13:51:49 +1000

    Configure AUXTeX for typesetting documents with TeX

--- custom-settings.el
+++ custom-settings.el
@@ -8,7 +8,9 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages (quote (counsel swiper try avy use-package which-key))))
+ '(package-selected-packages
+   (quote
+    (auctex counsel swiper try avy use-package which-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
