30a88b40ff9eef6b3a218f373c295a5a5de64f8a
Dileban Karunamoorthy <dileban@gmail.com>
2018-08-18 09:56:05 +1000

    Add external env.el file for loading system dependent variables

---
+++ env.el
@@ -0,0 +1,15 @@
+;;; env.el --- Set custom environment variables
+
+;;; Commentary:
+
+;; This file is only a template.  Edit this file to add custom
+;; environment variables that are system dependent.  This includes
+;; authentication tokens required by certain packages.  Changes to
+;; this files are not meant to be in git (see .gitignore).
+
+;;; Code:
+
+;; Examples:
+;; (setenv "MYAPP" "/home/dileban/bin/myapp")
+
+;;; env.el ends here
