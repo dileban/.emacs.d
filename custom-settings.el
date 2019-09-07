3648bf2b11635d01257e3d8ddba7074433212159
Dileban Karunamoorthy <dileban@gmail.com>
2019-09-07 12:12:30 +1000

    Update custom settings

--- custom-settings.el
+++ custom-settings.el
@@ -7,11 +7,11 @@
 	[default default default italic underline success warning error])
  '(custom-safe-themes
 	(quote
-	 ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+	 ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(helm-boring-file-regexp-list (quote (".git")))
  '(package-selected-packages
 	(quote
-	 (dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key))))
+	 (projectile ivy dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
