b899efbd6d9a06ddbf5a3bdaf41970885ca58d39
Dileban Karunamoorthy <dilebank@au1.ibm.com>
2020-11-12 22:03:41 +1100

    Add use of crux package, enable smarter movement of point

--- custom-settings.el
+++ custom-settings.el
@@ -3,15 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-	[default default default italic underline success warning error])
  '(custom-safe-themes
 	(quote
-	 ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+	 ("9e4db9122a0b7564a8573825975ac58d8b96b3076b358bd2a9f40cd6bc8d271d" "46b2d7d5ab1ee639f81bde99fcd69eb6b53c09f7e54051a591288650c29135b0" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(helm-boring-file-regexp-list (quote (".git")))
  '(package-selected-packages
 	(quote
-	 (multiple-cursors cargo lsp-mode company rust-mode hindent hindent-mode dracula-theme haskell-mode projectile ivy dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key))))
+	 (color-theme-sanityinc-tomorrow afternoon-theme crux multiple-cursors cargo lsp-mode company rust-mode hindent hindent-mode dracula-theme haskell-mode projectile ivy dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
