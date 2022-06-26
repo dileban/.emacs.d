e3476d7fc9ce140a2a7660ce3ef76eed7d47eddc
Dileban Karunamoorthy <dileban@gmail.com>
2022-06-27 08:38:01 +1000

    Set minimum warning level to 'error'

--- custom-settings.el
+++ custom-settings.el
@@ -4,12 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	(quote
-	 ("9e4db9122a0b7564a8573825975ac58d8b96b3076b358bd2a9f40cd6bc8d271d" "46b2d7d5ab1ee639f81bde99fcd69eb6b53c09f7e54051a591288650c29135b0" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(helm-boring-file-regexp-list (quote (".git")))
+	'("1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "b8929cff63ffc759e436b0f0575d15a8ad7658932f4b2c99415f3dde09b32e97" "99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" "c968804189e0fc963c641f5c9ad64bca431d41af2fb7e1d01a2a6666376f819c" "3e34e9bf818cf6301fcabae2005bba8e61b1caba97d95509c8da78cff5f2ec8e" "fc7fd2530b82a722ceb5b211f9e732d15ad41d5306c011253a0ba43aaf93dccc" "1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "cabc32838ccceea97404f6fcb7ce791c6e38491fd19baa0fcfb336dcc5f6e23c" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "191bc4e53173f13e9b827272fa39be59b7295a0593b9f070deb6cb7c3745fd1d" "9e4db9122a0b7564a8573825975ac58d8b96b3076b358bd2a9f40cd6bc8d271d" "46b2d7d5ab1ee639f81bde99fcd69eb6b53c09f7e54051a591288650c29135b0" "eae831de756bb480240479794e85f1da0789c6f2f7746e5cc999370bbc8d9c8a" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
+ '(helm-boring-file-regexp-list '(".git"))
+ '(org-agenda-files
+	'("~/Projects/github.com/dileban/journal/roam/20210901220021-types_of_stablecoins.org"))
  '(package-selected-packages
-	(quote
-	 (highlight-indent-guides-mode flycheck-yamllint highlight-indent-guides highlight-indentation color-theme-sanityinc-tomorrow afternoon-theme crux multiple-cursors cargo lsp-mode company rust-mode hindent hindent-mode dracula-theme haskell-mode projectile ivy dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key))))
+	'(org-roam-ui org-noter simple-httpd websocket org-roam-bibtex org-ref helm-bibtex org-roam lsp-treemacs lsp-ui monokai-theme highlight-indent-guides-mode flycheck-yamllint highlight-indent-guides highlight-indentation color-theme-sanityinc-tomorrow afternoon-theme crux multiple-cursors cargo lsp-mode company rust-mode hindent hindent-mode dracula-theme haskell-mode projectile ivy dockerfile-mode protobuf-mode dashboard magit markdown-mode yaml-mode racket-mode helm-projectile unfill org-bullets go-guru yasnippet expand-region go-autocomplete auto-complete flycheck helm-swoop go-mode solidity-mode helm auctex counsel swiper try avy use-package which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
